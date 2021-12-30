bded3a1e672051a8a62c90068d64f4f16798a472
manuel dellagala <manuel.dellagala@gmail.com>
2021-12-30 19:50:59 +0100

    posizioni caselle

--- src/Board.js
+++ src/Board.js
@@ -6,7 +6,7 @@ import Square from './Square'
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
--- src/Square.js
+++ src/Square.js
@@ -1,14 +1,13 @@
 import React from "react"
+import './Square.css'
 
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
-                {
-
-                }
+            <button className="square" onClick={()=>{console.log(this.props.value)}}>
+                {this.props.value}
             </button>
-        )
+        );
     }
 }
 
