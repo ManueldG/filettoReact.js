5044677f1ecac37565b1c89747d4f624fa77a723
manuel dellagala <manuel.dellagala@gmail.com>
2021-12-30 18:48:24 +0100

    components

--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,16 @@
 import React from "react"
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        
+    render() {
+        return (
+            <button className="square">
+                {
+
+                }
+            </button>
+        )
     }
 }
 
 export default Square;
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Game from './Game';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    
-    <Board />
+
+    <Game />
 
   </React.StrictMode>,
   document.getElementById('root')
