a9c96441995e85c3363927e5ec4cef44505b1891
manuel dellagala <manuel.dellagala@gmail.com>
2022-01-01 11:01:08 +0100

    turno

--- src/Board.js
+++ src/Board.js
@@ -5,31 +5,54 @@ import Square from './Square'
 
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      next: true,
+    }
+  }
+
+ handleClick(i){
+  const squares = this.state.squares.slice();
+  squares[i] = this.state.next ? 'X' : 'O';
+  this.setState({
+    squares : squares,
+    next : !this.state.next,
+  });
+ }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+    <Square value={this.state.squares[i]} onClick={()=>{this.handleClick(i)}}/>
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    
+    
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+        <div className="status">Next player:{ this.state.next? 'X' : 'O' }</div>
+        <div className="board">
+          <div className="board-row">
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            {this.renderSquare(3)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            {this.renderSquare(6)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            {this.renderSquare(9)}
+          </div>
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        
       </div>
     );
   }
--- src/Square.js
+++ src/Square.js
@@ -1,15 +1,14 @@
+
 import React from "react"
 import './Square.css'
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square" onClick={()=>{console.log(this.props.value)}}>
-                {this.props.value}
-            </button>
-        );
-    }
-}
-
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  
 export default Square;
 
