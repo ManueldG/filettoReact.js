50ae9dc98eddbbb4f143408764ced7422b3d68cd
manuel dellagala <manuel.dellagala@gmail.com>
2022-01-07 11:45:27 +0100

    divisione componenti

--- src/Board.js
+++ src/Board.js
@@ -3,9 +3,13 @@ import './Board.css';
 
 
 
-
-
-
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 class Board extends React.Component { 
 
   renderSquare(i) {
@@ -17,34 +21,11 @@ class Board extends React.Component {
     );
   }
 
-  vincita(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  render() {
+  render() {   
     
-    
-    console.log(this.vincita());
     return (
       <div>
-        <div className="status">Next player:{ this.state.next? 'X' : 'O' }</div>
-        <div className="status">{ this.vincita() ? 'the winner is '+this.vincita() : '' }</div>
+        
         <div className="board">
           <div className="board-row">
             {this.renderSquare(0)}
@@ -62,14 +43,11 @@ class Board extends React.Component {
             {this.renderSquare(8)}
           </div>
         </div>
-        <div className="cron">
-          
-        </div>
+        
       </div>
     );
   }
 }
-
 
 
 export default Board;
--- src/Game.js
+++ src/Game.js
@@ -1,119 +1,136 @@
 import React from "react"
-import Board from './Board'
-import Square from "./Square";
+import Board from './Board';
 
 
 
-class Game extends React.Component {
 
-    constructor(props) {
-        super(props);
-    
-        this.state = {
 
-          cron:[
-                  {
-                    squares: Array(9).fill(null)//scacchiera 
-                  }
-                ], // cronologia mosse
-          
-          next: true, // prossimo giocatore X?    
-          stepNumber: 0      
-          
-        }
+/**
+ * Game
+ */
+ class Game extends React.Component {
+
+  constructor(props) {
+      super(props);
+  
+      this.state = {
+
+        cron:[
+                {
+                  squares: Array(9).fill(null) //scacchiera 
+                }
+              ], // cronologia mosse
+        
+        next: true, // prossimo giocatore X?    
+        stepNumber: 0      
+        
       }
+      console.log("constructor",this.state);
+    }
 
-      
-      handleClick(i){
+    
+    handleClick(i){
+      console.log("click",this.state);
+      const cron = this.state.cron.slice(0,this.state.stepNumber + 1);
+      const current = cron[cron.length - 1];
+      const squares = current.squares.slice(); // copia dati in squares
+      console.log("click2",this.state);
+      if (squares[i] || vincita(squares))
+        return;
+    
+      squares[i] = this.state.next ? 'X' : 'O';
 
-        const squares = this.state.squares.slice(); // copia dati in squares
-        const  = cron[cron.length - 1];
-        const cron = this.state.cron.slice(0,this.state.stepNumber + 1);
-         
-        if (squares[i] || this.vincita(squares))
-          return;
-      
-        squares[i] = this.state.next ? 'X' : 'O';
-
-        this.setState({
-          cron : cron.concat([{
+      this.setState({
+        cron : cron.concat([
+          {
             squares:squares
           }
-        ]),
-          next : !this.state.next,
-          stepNumber: cron.length,
-          
-        });
-        console.log(this.state);
-
-    }
-      renderSquare(i) {
-    
-        return (
-        <Square value={this.props.squares[i]} onClick={()=>{this.props.onClick(i)}}/>
-        );
-    
-      }
-
-      jumpTo(step) {
-        this.setState({
-          stepNumber: step,
-          xIsNext: (step % 2) === 0
-        });
-      }
-
-      vincita(squares) {
-        const lines = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6]
-        ];
-        for (let i = 0; i < lines.length; i++) {
-          const [a, b, c] = lines[i];
-          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-          }
-        }
-        return null;
-      }
+      ]),
+        next : !this.state.next,
+        stepNumber: cron.length,
+        
+      });
       
+
+  }
+
+  jumpTo(step) {
+    console.log(this.state);
+    this.setState({
+      stepNumber: step,
+      next: (step % 2) === 0
+    });
+  }   
+  
+  render() {      
     
-    render() {      
-        console.log(this.state.cron);
-        const cron = this.state.cron;
-        const current = cron[cron.length - 1];
-        const winner = this.vincita(current.squares);   
-        let status;
-        if (winner) {     
-            status = 'Winner: ' + winner;
-        } else {      
-            status = 'Next player: ' + (this.state.next ? 'X' : 'O');    }
+      const cron = this.state.cron;
+      const current = cron[this.state.stepNumber];
+      const winner = vincita(current.squares);   
+      console.log("render",this.state);
+      let status;
 
-        return(
-        <div className="game">
-            <div className="game-board">
-                <Board 
-                squares={current.squares} 
-                onClick={(i) => this.handleClick(i)}
-                />
-            </div>
-            <div className="game-info">
-                <div>
+      if (winner) {     
+          status = 'Winner: ' + winner;
+      } else {      
+          status = 'Next player: ' + (this.state.next ? 'X' : 'O');    
+        }       
 
-                </div>
-                <ol>
+      const moves = cron.map((val,index)=>{
+        const txt = val ? 'ritorna alla mossa '+ index : 'ricomincia';
+        return (
+          <li key={index}>
+              <button onClick={() => this.jumpTo(index)}>{txt}</button>
+          </li>
+          )
+        }
 
-                </ol>
-            </div>
-        </div>
-        )
-    }
+      )
+
+      return(
+      <div className="game">
+          <div className="game-board">
+            {status}
+              <Board 
+              squares={current.squares} 
+              onClick={(i) => this.handleClick(i)}
+              />
+          </div>
+          <div className="game-info">
+            
+              <div>
+
+              </div>
+              <ol>
+                {moves}
+              </ol>
+          </div>
+      </div>
+      )
+  }
 }
+
+function vincita(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 
 export default Game
 
